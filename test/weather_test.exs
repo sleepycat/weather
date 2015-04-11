7f04670448a67258aa3967b0300bb58537c63d3d
Mike Williamson <mike@korora.ca>
2015-04-10 23:36:32 -0400

    Use Exunits contexts to reduce duplication

--- test/weather_test.exs
+++ test/weather_test.exs
@@ -9,20 +9,23 @@ end
 defmodule WeatherTest do
   use ExUnit.Case
 
+  setup do
+    {:ok, response_data: File.read!(Path.expand("./test/data/weather.json"))}
+  end
+
   test "returns a map of details for a given city" do
       forecast = Weather.in_city("Ottawa", FakeHTTPClient)
       assert %{high: 3.544, low: 3.544, city: "Ottawa", description: "broken clouds"} = forecast
   end
 
-  test "creates a summary map of a response" do
+  test "creates a summary map of a response", context do
     parsed_json =
-    Poison.Parser.parse!(File.read!(Path.expand("./test/data/weather.json")))
+    Poison.Parser.parse!(context[:response_data])
     assert %{high: _, low: _, city: _, description: _} = Weather.summary_map parsed_json
   end
 
-  test "produces json from a string" do
-    raw_json = File.read! Path.expand("./test/data/weather.json")
-    data = Weather.parse_response(raw_json)
+  test "produces json from a string", context do
+    data = Weather.parse_response(context[:response_data])
     weather_id = data["id"]
     assert weather_id = 6094817
   end
